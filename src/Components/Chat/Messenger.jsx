6c953b05159843244817d049c0d5e0d0ef2e467c
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-02-18 10:04:08 -0500

    messenger scroll no

--- src/Components/Chat/Messenger.jsx
+++ src/Components/Chat/Messenger.jsx
@@ -135,7 +135,7 @@ export default function Messenger({ visible, contactos, user }) {
     <Grid container direction="row" justifyContent="center" alignItems="start">
       <Grid xs item={true}>
         <Container
-          sx={{ maxHeight: "90vh", overflow: "hidden", height: "90vh" }}
+          sx={{ maxHeight: "80vh", overflow: "hidden", height: "80vh" }}
         >
           <MessagesList conversations={conversations} user={user} />
         </Container>
@@ -147,7 +147,7 @@ export default function Messenger({ visible, contactos, user }) {
       </Grid>
       <Grid xs item={true}>
         <Container
-          sx={{ maxHeight: "90vh", overflow: "hidden", height: "90vh" }}
+          sx={{ maxHeight: "80vh", overflow: "hidden", height: "80vh" }}
         >
           <FriendList online={online} offline={offline} />
         </Container>
